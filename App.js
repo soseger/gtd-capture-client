a69ae1022abed96f2d93f1b837c6c454b595ab63
soseger <katja.seger+github@shs.fi>
2019-09-25 20:34:04 +0300

    Customized tab bar icons

--- App.js
+++ App.js
@@ -2,12 +2,29 @@ import React from "react";
 import { Text, View } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import Icon from "react-native-vector-icons/Ionicons";
 import CaptureView from "./components/CaptureView";
 import SettingsView from "./components/SettingsView";
 
-const TabNavigator = createBottomTabNavigator({
-  Home: CaptureView,
-  Settings: SettingsView
-});
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: CaptureView,
+    Settings: SettingsView
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        const iconName =
+          routeName === "Settings" ? "ios-settings" : "ios-paper";
+        return <Icon name={iconName} size={32} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: "blue",
+      inactiveTintColor: "gray"
+    }
+  }
+);
 
 export default createAppContainer(TabNavigator);
