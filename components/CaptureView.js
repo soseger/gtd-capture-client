cfbe88e8d0ee5bbfc15c80a38ffd3acf28978f0d
soseger <katja.seger+github@shs.fi>
2019-09-28 16:21:47 +0300

    Implemented CaptureView

--- components/CaptureView.js
+++ components/CaptureView.js
@@ -1,10 +1,36 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Alert, Button, TextInput, Text, View } from "react-native";
+import axios from "axios";
+import config from "../config";
 
-export default function CaptureView() {
+export default function CaptureView({ screenProps }) {
+  const [message, onMessageChange] = React.useState("");
+
+  function handlePress() {
+    axios
+      .post(config.url, {
+        email: screenProps.email,
+        message: message
+      })
+      .then(() => {
+        Alert.alert("Message sent!", "", [{ text: "OK" }]);
+        onMessageChange("");
+      })
+      .catch(error => {
+        console.error(error);
+        Alert.alert("Sending message failed", "", [{ text: "OK" }]);
+      });
+  }
+
   return (
     <View>
-      <Text>Capture View</Text>
+      <Text>GET THINGS DONE</Text>
+      <TextInput
+        placeholder="Type something"
+        onChangeText={text => onMessageChange(text)}
+        value={message}
+      />
+      <Button title="Send" onPress={handlePress} />
     </View>
   );
 }
