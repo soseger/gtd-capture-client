71981f5b99d01e2590fa65f2c69a82d9e37500ad
soseger <katja.seger+github@shs.fi>
2019-09-28 15:07:31 +0300

    Cleaned up unnecessary code

--- utils/Store.js
+++ utils/Store.js
@@ -2,12 +2,6 @@ import * as SecureStore from "expo-secure-store";
 
 const EMAIL_KEY = "GTD_EMAIL_KEY";
 
-export function hasEmail() {
-  return getEmail().then(email => {
-    return Boolean(email);
-  });
-}
-
 export function setEmail(email) {
   return SecureStore.setItemAsync(EMAIL_KEY, email);
 }
