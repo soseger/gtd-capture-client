cca8a5a28912d3b5f7021efb8a63e5a7b3a2d416
soseger <katjasofiaseger@gmail.com>
2019-09-29 16:51:01 +0300

    Added shared secret to axios request

--- components/CaptureView.js
+++ components/CaptureView.js
@@ -1,24 +1,31 @@
-import React from "react";
-import { Alert, Button, TextInput, Text, View } from "react-native";
-import axios from "axios";
-import config from "../config";
+import React from 'react';
+import { Alert, Button, TextInput, Text, View } from 'react-native';
+import axios from 'axios';
+import config from '../config';
+import { API_SECRET } from 'react-native-dotenv';
 
 export default function CaptureView({ screenProps }) {
-  const [message, onMessageChange] = React.useState("");
+  const [message, onMessageChange] = React.useState('');
 
   function handlePress() {
+    const headers = {
+      'x-api-secret': API_SECRET
+    };
+
+    const data = {
+      email: screenProps.email,
+      message: message
+    };
+
     axios
-      .post(config.url, {
-        email: screenProps.email,
-        message: message
-      })
+      .post(config.url, data, { headers: headers })
       .then(() => {
-        Alert.alert("Message sent!", "", [{ text: "OK" }]);
-        onMessageChange("");
+        Alert.alert('Message sent!', '', [{ text: 'OK' }]);
+        onMessageChange('');
       })
       .catch(error => {
         console.error(error);
-        Alert.alert("Sending message failed", "", [{ text: "OK" }]);
+        Alert.alert('Sending message failed', '', [{ text: 'OK' }]);
       });
   }
 
